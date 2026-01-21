341b320c07c0d9f7c19eed57e9acdb1bf0c598e6
awoel74 <herilaza.woel-lala@eleve.isep.fr>
2026-01-21 10:12:44 +0100

    Update schema.sql

--- schema.sql
+++ schema.sql
@@ -1,8 +1,31 @@
-DROP TABLE IF EXISTS clients;
-CREATE TABLE clients (
+
+DROP TABLE IF EXISTS loans;
+DROP TABLE IF EXISTS books;
+DROP TABLE IF EXISTS users;
+
+
+CREATE TABLE users (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
-    created TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    nom TEXT NOT NULL,
-    prenom TEXT NOT NULL,
-    adresse TEXT NOT NULL
+    username TEXT NOT NULL UNIQUE,
+    role TEXT NOT NULL
+);
+
+
+CREATE TABLE books (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    title TEXT NOT NULL,
+    author TEXT NOT NULL,
+    stock_total INTEGER NOT NULL,
+    stock_available INTEGER NOT NULL
+);
+
+
+CREATE TABLE loans (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    user_id INTEGER NOT NULL,
+    book_id INTEGER NOT NULL,
+    loan_date TEXT NOT NULL,
+    return_date TEXT,
+    FOREIGN KEY (user_id) REFERENCES users(id),
+    FOREIGN KEY (book_id) REFERENCES books(id)
 );
