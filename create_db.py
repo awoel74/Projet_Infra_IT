0b1946eeb39ce3c656285e965865c262f25b2b5d
awoel74 <herilaza.woel-lala@eleve.isep.fr>
2026-01-21 10:13:35 +0100

    Update create_db.py

--- create_db.py
+++ create_db.py
@@ -7,14 +7,25 @@ with open('schema.sql') as f:
 
 cur = connection.cursor()
 
-cur.execute("INSERT INTO clients (nom, prenom, adresse) VALUES (?, ?, ?)",('DUPONT', 'Emilie', '123, Rue des Lilas, 75001 Paris'))
-cur.execute("INSERT INTO clients (nom, prenom, adresse) VALUES (?, ?, ?)",('LEROUX', 'Lucas', '456, Avenue du Soleil, 31000 Toulouse'))
-cur.execute("INSERT INTO clients (nom, prenom, adresse) VALUES (?, ?, ?)",('MARTIN', 'Amandine', '789, Rue des Érables, 69002 Lyon'))
-cur.execute("INSERT INTO clients (nom, prenom, adresse) VALUES (?, ?, ?)",('TREMBLAY', 'Antoine', '1010, Boulevard de la Mer, 13008 Marseille'))
-cur.execute("INSERT INTO clients (nom, prenom, adresse) VALUES (?, ?, ?)",('LAMBERT', 'Sarah', '222, Avenue de la Liberté, 59000 Lille'))
-cur.execute("INSERT INTO clients (nom, prenom, adresse) VALUES (?, ?, ?)",('GAGNON', 'Nicolas', '456, Boulevard des Cerisiers, 69003 Lyon'))
-cur.execute("INSERT INTO clients (nom, prenom, adresse) VALUES (?, ?, ?)",('DUBOIS', 'Charlotte', '789, Rue des Roses, 13005 Marseille'))
-cur.execute("INSERT INTO clients (nom, prenom, adresse) VALUES (?, ?, ?)",('LEFEVRE', 'Thomas', '333, Rue de la Paix, 75002 Paris'))
+# Utilisateurs
+cur.execute("INSERT INTO users (username, role) VALUES (?, ?)", ('admin', 'admin'))
+cur.execute("INSERT INTO users (username, role) VALUES (?, ?)", ('user', 'user'))
+
+# Livres (données de test)
+cur.execute(
+    "INSERT INTO books (title, author, stock_total, stock_available) VALUES (?, ?, ?, ?)",
+    ('1984', 'George Orwell', 5, 5)
+)
+cur.execute(
+    "INSERT INTO books (title, author, stock_total, stock_available) VALUES (?, ?, ?, ?)",
+    ('Le Petit Prince', 'Antoine de Saint-Exupéry', 3, 3)
+)
+cur.execute(
+    "INSERT INTO books (title, author, stock_total, stock_available) VALUES (?, ?, ?, ?)",
+    ('L’Étranger', 'Albert Camus', 4, 4)
+)
 
 connection.commit()
 connection.close()
+
+print("Base bibliothèque créée")
